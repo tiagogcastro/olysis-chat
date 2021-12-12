ceaa9b0be642923e615b39fb0deb9d0d5f4ff32c
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-12 13:52:09 -0300

    feat: Using message routers

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 import { user } from '@modules/user/routes/user_routers';
 import { auth } from '@modules/user/routes/auth_routers';
+import { message } from '@modules/message/routes/message_routers';
 
 export const router = Router();
 
 router.use('/users', user);
 router.use('/auth', auth);
+router.use('/message', message);
