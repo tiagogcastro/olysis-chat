a71f866fd2dc073ef80bd5dd2987aeb685b65fd9
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-12 15:36:01 -0300

    feat: Create router to list messages by user logged with receive

--- backend/src/modules/message/routes/message_routers.ts
+++ backend/src/modules/message/routes/message_routers.ts
@@ -3,11 +3,14 @@ import { Router } from 'express';
 import { ensureAuthenticated } from '@modules/user/middlewares/ensureAuthenticated';
 
 import { 
-  CreateMessageController, 
+  CreateMessageController,
+  UserMessagesWithReceiveUserController
 } from '../controllers';
 
 export const message = Router();
 
 const createMessageController = new CreateMessageController();
+const userMessagesWithReceiveUserController = new UserMessagesWithReceiveUserController();
 
 message.post('/create', ensureAuthenticated, createMessageController.execute);
+message.get('/list-all/:receive_user_id', ensureAuthenticated, userMessagesWithReceiveUserController.execute);
