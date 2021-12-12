4ca859de152ad48510ece0723c184057789deda5
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-12 11:10:52 -0300

    feat: Exporting controllers and services

---
+++ backend/src/modules/user/controllers/index.ts
@@ -0,0 +1,4 @@
+export * from './RegisterUserController';
+export * from './AppAuthUserController';
+export * from './GetUserLoggedController';
+export * from './ListAllUsersExceptLoggedController';
---
+++ backend/src/modules/user/services/index.ts
@@ -0,0 +1,4 @@
+export * from './RegisterUserService';
+export * from './AppAuthUserService';
+export * from './GetUserLoggedService';
+export * from './ListAllUsersExceptLoggedService';
